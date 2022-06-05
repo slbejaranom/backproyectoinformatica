a42bf6c50ef5b5dabd581c6c3cde26ccf67af0c2
Sebastian Bejarano <slbejaranom@gmail.com>
2022-06-05 18:24:29 -0500

    Agregar GET pedidos

--- app.py
+++ app.py
@@ -50,3 +50,10 @@ class PedidoReceta(db.Model):
     receta = db.Column(db.Integer, db.ForeignKey("receta.id"))
 
 db.create_all()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug = True)
+
+@app.route("pedidos", methods=["GET"])
+def obtenerPedidos():
+    pedidos = Pedido.query.all()
